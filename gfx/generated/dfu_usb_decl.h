3838f977b3238cd58d2ba008a2461246abb14ccb
mean <fixounet@free.fr>
2022-04-23 08:41:56 +0200

    add splash icon

---
+++ gfx/generated/dfu_usb_decl.h
@@ -0,0 +1,3 @@
+#define dfu_usb_width 96
+#define dfu_usb_height 112
+extern const unsigned char dfu_usb[];
--- src/lcd.cpp
+++ src/lcd.cpp
@@ -9,13 +9,15 @@ extern "C"
 {
 #include "ili9341_stm32_parallel8.h"
 }
+#include "../gfx/generated/dfu_usb_decl.h"
+#include "../gfx/generated/dfu_usb_compressed.h"
 #define ST7735_BUFFER_SIZE_WORD 128
 void drawHSBitmap(int widthInPixel, int height, int wx, int wy, int fgcolor, int bgcolor, const uint8_t *data)
 {
     iliHS hs(data);
     int nbPixel=widthInPixel*height;
     int pixel=0;
-    ili_set_address_window(wx, wy,  widthInPixel+wx, height+height);
+    ili_set_address_window(wx, wy,  widthInPixel+wx-1, height+height-1);
     int mask=0;
     int cur;
     uint16_t temp[ST7735_BUFFER_SIZE_WORD];
@@ -63,7 +65,7 @@ void runLcd()
   ili_rotate_display(1);
   ili_fill_screen(ILI_COLOR_CYAN);
 
-  drawHSBitmap(16,16,100,100,0xff,0,data);
+  drawHSBitmap(dfu_usb_width,dfu_usb_height,(320-dfu_usb_width)/2,(240-dfu_usb_height)/2,0xff,0,dfu_usb);
 
 }
 
